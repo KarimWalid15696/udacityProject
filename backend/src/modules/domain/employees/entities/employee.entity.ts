3ef6c7f350c1d665b480fc3d7868ab2f25dd19d6
Karim Walid <Kwalid@citglobal.com>
2021-09-19 00:09:20 +0200

    modify

--- backend/src/modules/domain/employees/entities/employee.entity.ts
+++ backend/src/modules/domain/employees/entities/employee.entity.ts
@@ -120,7 +120,7 @@ export enum SalaryType {
   HOURLY = 'hourly',
 }
 
-export function getDateFromString(date: string): Date {
+export function getDateFromString(date: string): string {
   return moment(date).format('M/D/YYYY');
 }
 
